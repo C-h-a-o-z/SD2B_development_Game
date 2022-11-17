42321152691184feb000310be62f7a22f1c96111
C-h-a-o-z <b.somers@sintlucasedu.nl>
2022-11-17 12:58:20 +0100

    WorkingMovement

---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+	[SerializeField] private LayerMask m_WhatIsGround;
+	[SerializeField] private Transform groundCheck;
+	public bool onGround = false;
+
+	public float speed = 10;
+    public float jumpForce = 100;
+	const float k_GroundedRadius = .2f;
+	public int jumpAmount = 1;
+	private Rigidbody2D rigBody2D;
+
+    private void Awake()
+    {
+		rigBody2D = GetComponent<Rigidbody2D>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        float dirX = Input.GetAxis("Horizontal");
+        transform.Translate(transform.right * dirX * speed * Time.deltaTime);
+
+		if (Input.GetKeyDown(KeyCode.Space))
+        {
+			if (onGround == true)
+			{
+				onGround = false;
+				rigBody2D.AddForce(new Vector2(0f, jumpForce));
+			}
+			else if (jumpAmount > 0)
+			{
+				jumpAmount = jumpAmount - 1;
+				rigBody2D.velocity = new Vector2(rigBody2D.velocity.x, 0);
+				rigBody2D.AddForce(new Vector2(0f, jumpForce));
+			}
+			else
+            {
+				Debug.Log("NoRemainingJumps");
+            }
+
+        }
+    }
+	private void FixedUpdate()
+	{
+		bool wasGrounded = onGround;
+		onGround = false;
+		Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheck.position, k_GroundedRadius, m_WhatIsGround);
+		for (int i = 0; i < colliders.Length; i++)
+		{
+			if (colliders[i].gameObject != gameObject)
+			{
+				onGround = true;
+				jumpAmount = 1;
+			}
+		}
+	}
+}
